f6623a1867d1ef2b7ab98ff6ee6cf9fecf87d36e
jeffrey <jcouch210@gmail.com>
2025-07-07 18:58:07 -0700

    Add a comment with the font's aspect ratio

--- src/interface/window.rs
+++ src/interface/window.rs
@@ -14,6 +14,7 @@ use crate::simulation::{
 
 use super::text_editor::TextEditor;
 
+/// The width of each character should be 0.6 times the font size
 pub static FONT: LazyLock<Font> = LazyLock::new(|| {
     text::load_ttf_font_from_bytes(&fs::read("assets/CommitMonoNerdFontMono-Regular.otf").unwrap())
         .unwrap()
